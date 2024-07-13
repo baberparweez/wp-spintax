435453c06483c7f060bce9d2fd6d691ae4621c05
baberparweez <baberparweez@gmail.com>
2024-07-13 14:05:54 -0300

    change plugin functionality to use JS instead - to avoid page builder clashes with php

--- public/class-spintax-public.php
+++ public/class-spintax-public.php
@@ -1,189 +1,139 @@
 <?php
 
-/**
- * The public-facing functionality of the plugin.
- *
- * @link       -
- * @since      1.0.0
- *
- * @package    Spintax
- * @subpackage Spintax/public
- */
-
-/**
- * The public-facing functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the public-facing stylesheet and JavaScript.
- *
- * @package    Spintax
- * @subpackage Spintax/public
- * @author     Baber Parweez <baberparweez@gmail.com>
- */
 class Spintax_Public
 {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
 	private $plugin_name;
-
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
 	private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
 	public function __construct($plugin_name, $version)
 	{
-
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_styles()
 	{
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Spintax_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Spintax_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/spintax-public.css', array(), $this->version, 'all');
+		if (!is_admin() && !defined('DOING_AJAX') && !$this->is_builder_context()) {
+			wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/spintax-public.css', array(), $this->version, 'all');
+			// Add inline style to hide the body content initially
+			wp_add_inline_style($this->plugin_name, 'body { visibility: hidden; }');
+		}
 	}
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_scripts()
 	{
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Spintax_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Spintax_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/spintax-public.js', array('jquery'), $this->version, false);
+		if (!is_admin() && !defined('DOING_AJAX') && !$this->is_builder_context()) {
+			wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/spintax-public.js', array('jquery'), $this->version, true);
+		}
 	}
 
-	/**
-	 * Adds the spintax functionality to text editors
-	 *
-	 * @return null
-	 */
+	private function is_builder_context()
+	{
+		global $post;
+
+		// Check for Bricks Builder
+		if (isset($_GET['bricks']) && $_GET['bricks'] === 'builder') {
+			return true;
+		}
+
+		// Check for Elementor
+		if (function_exists('Elementor\Plugin') && \Elementor\Plugin::$instance->preview->is_preview_mode() && \Elementor\Plugin::$instance->db->is_built_with_elementor($post->ID)) {
+			return true;
+		}
+		if (function_exists('Elementor\Plugin') && \Elementor\Plugin::$instance->preview->is_edit_mode() && \Elementor\Plugin::$instance->db->is_built_with_elementor($post->ID)) {
+			return true;
+		}
+
+		// Check for Gutenberg
+		if (is_admin() && function_exists('get_current_screen')) {
+			$screen = get_current_screen();
+			if ($screen && $screen->is_block_editor) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	public function spintax()
 	{
-		// Pass in the string you'd for which you'd like a random output
-		function random($str)
-		{
-			// Returns random values found between { this | and }
-			$content = preg_replace_callback("/{(.*?)}/", function ($match) {
-				// Splits 'foo|bar' strings into an array
-				$words = explode("|", $match[1]);
-				// Grabs a random array entry and returns it
-				return $words[array_rand($words)];
-				// The input string, which you provide when calling this func
-			}, $str);
-
-			return $content;
-		}
-
-		// Apply spintax filter to body text using buffer if not in admin or builder context
-		if (!is_admin() && !defined('DOING_AJAX')) {
-			ob_start(function ($buffer) {
-				// Use regex to only target the body content
-				$pattern = '/<body[^>]*>(.*?)<\/body>/is';
-				return preg_replace_callback($pattern, function ($matches) {
-					return '<body>' . random($matches[1]) . '</body>';
-				}, $buffer);
-			});
-		}
-	}
-
-	/**
-	 * Adds the dynamic spintax functionality to text editors
-	 *
-	 * @return null
-	 */
-	public function js_spintax()
-	{
-		function js_random($str)
-		{
-			$content = preg_replace_callback("/~(.*?)~/", function ($match) {
-				$words = explode("|", $match[1]);
-				$span = '<span class="spintax">' . $words[0] . '<noscript>' . implode('|', $words) . '</noscript></span>';
-				return $span;
-			}, $str);
-
-			return $content;
-		}
-
-		// Apply spintax filter to body text using buffer
-		if (!is_admin() && !defined('DOING_AJAX')) {
-			ob_start(function ($buffer) {
-				// Use regex to only target the body content
-				$pattern = '/<body[^>]*>(.*?)<\/body>/is';
-				return preg_replace_callback($pattern, function ($matches) {
-					return '<body>' . js_random($matches[1]) . '</body>';
-				}, $buffer);
-			});
-
-			// Add JavaScript to handle the dynamic spintax replacement
+		// Add JavaScript to handle the spintax transformations
+		if (!is_admin() && !defined('DOING_AJAX') && !$this->is_builder_context()) {
 			add_action('wp_footer', function () {
 ?>
 				<script type="text/javascript">
-					jQuery(document).ready(function($) {
-						var fadeSpeed = 350;
-						$('.spintax').each(function() {
-							var spintaxElement = $(this);
-							var fullSpintax = spintaxElement.find('noscript').text();
-							var spintaxArr = fullSpintax.split('|');
-							var i = 0;
+					document.addEventListener('DOMContentLoaded', function() {
+						function random(str) {
+							return str.replace(/{(.*?)}/g, function(match, p1) {
+								var words = p1.split("|");
+								return words[Math.floor(Math.random() * words.length)];
+							});
+						}
 
-							spintaxElement.html(spintaxArr[i]).fadeIn(fadeSpeed);
+						function js_random(str) {
+							return str.replace(/~(.*?)~/g, function(match, p1) {
+								var words = p1.split("|");
+								return '<span class="spintax">' + words[0] + '<noscript>' + words.join('|') + '</noscript></span>';
+							});
+						}
 
-							setInterval(function() {
-								i = (i + 1) % spintaxArr.length;
-								spintaxElement.fadeOut(fadeSpeed, function() {
-									spintaxElement.html(spintaxArr[i]).fadeIn(fadeSpeed);
-								});
-							}, 2500);
+						// Apply spintax to elements that are not part of the editor
+						document.querySelectorAll('body *:not(.elementor-editor-active):not(.wp-admin):not(.edit-post-visual-editor)').forEach(function(element) {
+							element.innerHTML = random(element.innerHTML);
+							element.innerHTML = js_random(element.innerHTML);
 						});
+
+						// Reveal the body content after transformations are done
+						document.body.style.visibility = 'visible';
+					});
+				</script>
+			<?php
+			});
+		}
+	}
+
+	public function js_spintax()
+	{
+		// Add JavaScript to handle the dynamic spintax replacement
+		if (!is_admin() && !defined('DOING_AJAX') && !$this->is_builder_context()) {
+			add_action('wp_footer', function () {
+			?>
+				<script type="text/javascript">
+					document.addEventListener('DOMContentLoaded', function() {
+						function js_random(str) {
+							return str.replace(/~(.*?)~/g, function(match, p1) {
+								var words = p1.split("|");
+								return '<span class="spintax">' + words[0] + '<noscript>' + words.join('|') + '</noscript></span>';
+							});
+						}
+
+						jQuery(document).ready(function($) {
+							var fadeSpeed = 350;
+							$('.spintax').each(function() {
+								var spintaxElement = $(this);
+								var fullSpintax = spintaxElement.find('noscript').text();
+								var spintaxArr = fullSpintax.split('|');
+								var i = 0;
+
+								spintaxElement.html(spintaxArr[i]).fadeIn(fadeSpeed);
+
+								setInterval(function() {
+									i = (i + 1) % spintaxArr.length;
+									spintaxElement.fadeOut(fadeSpeed, function() {
+										spintaxElement.html(spintaxArr[i]).fadeIn(fadeSpeed);
+									});
+								}, 2500);
+							});
+						});
+
+						// Apply js_random to elements that are not part of the editor
+						document.querySelectorAll('body *:not(.elementor-editor-active):not(.wp-admin):not(.edit-post-visual-editor)').forEach(function(element) {
+							element.innerHTML = js_random(element.innerHTML);
+						});
+
+						// Reveal the body content after transformations are done
+						document.body.style.visibility = 'visible';
 					});
 				</script>
 <?php
